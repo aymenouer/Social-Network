5e31a66e3fa737c4a816c4cd9d66a51d83ca2c4e
Aymen Ouerghui <aymen.ouerghui@esprit.tn>
2022-02-26 23:59:54 +0100

    Change in topbar Current user with context api

--- client/src/components/topbar/Topbar.jsx
+++ client/src/components/topbar/Topbar.jsx
@@ -1,7 +1,11 @@
 import "./topbar.css";
 import { Search, Person, Chat, Notifications } from "@material-ui/icons";
 import {Link} from "react-router-dom"
+import { useContext } from 'react';
+import { AuthContext } from './../../context/AuthContext';
 export default function Topbar() {
+  const {user } = useContext(AuthContext);
+  const PF = process.env.REACT_APP_PUBLIC_FOLDER;
   return (
     <div className="topbarContainer">
       <div className="topbarLeft">
@@ -39,8 +43,9 @@ export default function Topbar() {
             <span className="topbarIconBadge">1</span>
           </div>
         </div>
-
-    <img src="/assets/person/1.jpeg" alt="" className="topbarImg" />
+<Link to={`profile/${user.username}`} >
+    <img src={ user.profilePicture ? PF+user.profilePicture : PF+"person/avatar.png" } alt="" className="topbarImg" />
+</Link>
 
       </div>
     </div>
