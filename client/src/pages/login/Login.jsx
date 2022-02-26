cef32d93bd73cf4132b248df5df3252d0d72fc0b
Aymen Ouerghui <aymen.ouerghui@esprit.tn>
2022-02-26 23:47:00 +0100

    Change Register

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -8,14 +8,14 @@ import {CircularProgress} from "@material-ui/core"
 export default function Login() {
     const email=useRef();
     const password = useRef();
-    const {user,isFetching,error,dispatch} = useContext(AuthContext)
+    const {isFetching,dispatch} = useContext(AuthContext)
 
 
     const handleClick = (e) =>{
 e.preventDefault();
 loginCall({email:email.current.value,password:password.current.value},dispatch)
     }
-    console.log(user);
+
   return (
    <div className="login">
        <div className="loginWrapper">
@@ -28,7 +28,7 @@ loginCall({email:email.current.value,password:password.current.value},dispatch)
                <form className="loginBox" onSubmit={handleClick} >
                    <input required ref={email} placeholder="Email" type="email" className="loginInput" />
                    <input minLength="6" required ref={password} placeholder="Password" type="password" className="loginInput" />
-              <button className="loginButton" disabled={isFetching} >
+              <button type="submit" className="loginButton" disabled={isFetching} >
                   {isFetching ? <CircularProgress size="20px" color="white" />:"Log In"} 
                   </button>
               <span className="loginForgot">Forgot Password?</span>
--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,6 +1,38 @@
 import "./register.css"
-
+import { useRef } from 'react';
+import  axios  from 'axios';
+import {useHistory} from "react-router"
 export default function Register() {
+    const username=useRef();
+    const email=useRef();
+    const password = useRef();
+    const confirmpassword = useRef();
+    const history = useHistory();
+    const handleClick = async(e) =>{
+        e.preventDefault();
+        if (confirmpassword.current.value !== password.current.value){
+            confirmpassword.current.setCustomValidity("Passwords don't match!");
+        }
+        else{
+            const user = {
+                username:username.current.value,
+                email:email.current.value,
+                password:password.current.value
+            }
+            try {
+                 await axios.post('/auth/register',user);    
+                 history.push("/");
+            } catch (err) {
+                console.log(err);
+            }
+            
+
+
+        }
+     
+    }
+
+
   return (
    <div className="register">
        <div className="registerWrapper">
@@ -10,16 +42,16 @@ export default function Register() {
            </div>
            <div className="registerRight">
 
-               <div className="registerBox">
-                   <input placeholder="Username" type="text" className="registerInput" />
-                   <input placeholder="Email" type="text" className="registerInput" />
-                   <input placeholder="Password" type="text" className="registerInput" />
-                   <input placeholder="Password Again" type="text" className="registerInput" />
-              <button className="registerButton" >Sign Up </button>
+               <form className="registerBox" onSubmit={handleClick} >
+                   <input placeholder="Username" required ref={username} type="text" className="registerInput" />
+                   <input placeholder="Email" required ref={email} type="email" className="registerInput" />
+                   <input minLength="6" placeholder="Password" required ref={password} type="password" className="registerInput" />
+                   <input placeholder="Password Again" required ref={confirmpassword} type="password" className="registerInput" />
+              <button className="registerButton" type="submit" >Sign Up </button>
        
               <button className="registerRegisterButton">
                 Log into Account</button>
-               </div>
+               </form>
            </div>
        </div>
    </div>
